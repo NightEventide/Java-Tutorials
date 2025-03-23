5a39a6c294a0e892e116d8ad9f90853e01c72fd8
NightEventide <seanlee12312@gmail.com~>
2025-03-24 02:14:40 +0800

    First Commit

--- src/Week1/JavaApplication1.java
+++ src/Week1/JavaApplication1.java
@@ -14,6 +14,7 @@ public class JavaApplication1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Hi");
         // TODO code application logic here
     }
     
